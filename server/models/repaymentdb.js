0b0bbda51e9b38500a6aae08d17c11b5867e9e27
schella mugabekazi <schellamugabekazi762@gmail.com>
2019-05-16 16:26:33 +0200

    [bug-node-modules-166059786]ignore node modules

--- server/models/repaymentdb.js
+++
@@ -1,3 +0,0 @@
-const repayments=[];
-
-export default repayments;
--- server/routes/repaymentRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import repayment from '../controllers/reapayment/repayment';
-
-
-const router = express.Router();
-
-router.post('/api/v1/repayment/:id',repayment.rapayLoan);
-
-export default router;
