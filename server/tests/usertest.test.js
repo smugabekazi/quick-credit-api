a3a7d304ebf664f9641a5053b95f36812bbe5204
schella mugabekazi <schellamugabekazi762@gmail.com>
2019-05-17 14:37:49 +0200

    [chore #166083402] api unit tests

--- server/tests/usertest.test.js
+++ server/tests/usertest.test.js
@@ -49,5 +49,68 @@ describe('SIGNIN USER TEST', () => {
   });
  
 });
+describe('Apply for Loan ', () => {
+  it('User should apply for a loan', (done) => {
+    const loan = {
+      "email":"schellamuabekeazi76@gmail.com",
+	   "tenor":"10",
+	   "amount":"50000"
+    };
+
+    chai.request(server)
+      .post('/api/v1/loans')
+      .send(loan)
+      .end((err, res) => {
+        res.body.should.be.an('object');
+        res.body.should.have.status(201);
+        res.body.should.have.property('status');
+        // res.body.should.have.property('data');
+        // res.body.data.should.be.a('array');
+        done();
+      });
+  });
+ 
+});
+describe('get all loans', () => {
+  it('Admin should view all loans', (done) => {
+    
+    
+
+    chai.request(server)
+      .get('/api/v1/loans')
+      .send()
+      .end((err, res) => {
+        res.body.should.be.an('object');
+        res.body.should.have.status(200);
+        res.body.should.have.property('status');
+        // res.body.should.have.property('data');
+        // res.body.data.should.be.a('array');
+        done();
+      });
+  });
+ 
+});
+// describe('Admin should post repayment history', () => {
+//   it('Admin should update repayment history in a favor of client', (done) => {
+//     const updateloan = {
+//       paidAmount:"2000"
+//     };
+
+//     chai.request(server)
+//       .post('api/v1/repayment/1')
+//       .send(updateloan)
+//       .end((err, res) => {
+//         res.body.should.be.an('object');
+//         res.body.should.have.status(201);
+//         res.body.should.have.property('status');
+//         // res.body.should.have.property('data');
+//         // res.body.data.should.be.a('array');
+//         done();
+//       });
+//   });
+ 
+// });
+
+
 
 
